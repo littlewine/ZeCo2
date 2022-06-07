b1f452da90b0faf5ef4e8bbdf060754123e3faca
amkrasakis <a.m.krasakis@uva.nl>
2022-06-07 18:02:34 +0200

    update paths

--- paths.py
+++ paths.py
@@ -3,40 +3,19 @@ import platform
 
 if platform.system() == 'Darwin':
     print('Using paths for Macbook')
-    path_data_folder = '/Users/amkrasakis/data/'
-    # path_treccast20 = '/Users/amkrasakis/data/treccastweb/2020'
-    path_canard = '/Users/amkrasakis/data/CANARD_Release'
-    path_index = '/Users/amkrasakis/data/indexes/index-cast2019'
     path_project_data = '/Users/amkrasakis/data/0sConvDR'
     path_anserini = '/Users/amkrasakis/libs/anserini'
     path_project_base = '/Users/amkrasakis/cqa-rewrite'
 
 elif platform.system() == 'Linux':
     print('Using paths for slurm')
-    path_data_folder = '/ivi/ilps/personal/akrasak/data/'
-    path_anserini = '/home/akrasak/anserini'
-    path_index = '/ivi/ilps/personal/akrasak/data/indexes/cast2019/index-cast2019'
-    path_project_data = '/ivi/ilps/personal/akrasak/data/0sConvDR'
-    # path_treccast20 = '/ivi/ilps/personal/akrasak/data/treccastweb/2020'
-    path_project_base = '/home/akrasak/cqa-rewrite'
+    path_anserini = '/home/akrasak/anserini' #TODO: change path to anserini (used for evaluation)
+    path_project_data = '/ivi/ilps/personal/akrasak/data/0sConvDR' # TODO: replace with 'data/'
+    path_project_base = '/home/akrasak/cqa-rewrite' # TODO: change with local path of current repo (where this file is located)
 
 path_treceval = os.path.join(path_anserini,'tools','eval','trec_eval.9.0.4','trec_eval')
 
 # Queries, qrels
-
-# path_queries_21 = {'json' :'/ivi/ilps/personal/akrasak/data/treccastweb/2021/2021_automatic_evaluation_topics_v1.0.json',
-#                     'queries_with_canonical': '/ivi/ilps/personal/akrasak/data/treccastweb/2021/queries_with_canonical.tsv',
-#                     'queries_with_canonical_256': '/ivi/ilps/personal/akrasak/data/treccastweb/2021/queries_with_canonical_256.tsv',
-#                     'queries_with_canonical_200': '/ivi/ilps/personal/akrasak/data/treccastweb/2021/queries_with_canonical_200.tsv',
-#                     'queries_with_canonical_186': '/ivi/ilps/personal/akrasak/data/treccastweb/2021/queries_with_canonical_186.tsv',
-#                    'queries_with_canonical_384': '/ivi/ilps/personal/akrasak/data/treccastweb/2021/queries_with_canonical_384.tsv',
-#
-#                    'queries_canonical_last': '/ivi/ilps/personal/akrasak/data/treccastweb/2021/queries_canonical_last.tsv',
-#                    'queries_history_only': '/ivi/ilps/personal/akrasak/data/treccastweb/2021/queries_history_only.tsv',
-#
-#                    'raw': '/ivi/ilps/personal/akrasak/data/treccastweb/2021/queries_raw.tsv',
-#                    'raw_SEP': '/ivi/ilps/personal/akrasak/data/treccastweb/2021/queries_raw_SEP.tsv',
-#                    }
 
 path_queries = {'cast19':
                     {'full_conv': os.path.join(path_project_data,'queries/2019', 'full_conv.tsv'),
@@ -88,20 +67,26 @@ path_qrels = {
 }
 
 # Collections
-path_collections = "/ivi/ilps/personal/akrasak/data/collections"
-paths_trecweb_cast21 = {'wapo': os.path.join(path_collections,'wapo.trecweb'),
-                        'marco': os.path.join(path_collections,'msmarco-docs.trecweb'),
-                        'kilt': os.path.join(path_collections,'kilt_knowledgesource.trecweb'),
-                        }
+path_collections = "data/collection_samples"
 
-path_collection_int = {'CAR':'/ivi/ilps/personal/akrasak/data/collections/car-wiki2020-01-01/Car_collection.tsv.int',
+# For CAsT21 I used the trecweb files provided from the organizers :
+# paths_trecweb_cast21 = {'wapo': os.path.join(path_collections,'wapo.trecweb'),
+#                         'marco': os.path.join(path_collections,'msmarco-docs.trecweb'),
+#                         'kilt': os.path.join(path_collections,'kilt_knowledgesource.trecweb'),
+#                         }
+
+path_collection_int = {
+                        # check examples in `data/collection_samples/msmarco-docs.tsv.sample.int`
+                        'CAR':'/ivi/ilps/personal/akrasak/data/collections/car-wiki2020-01-01/Car_collection.tsv.int',
                        'marcoP': '/ivi/ilps/personal/akrasak/data/collections/msmarco-passage/collection.tsv',
                        'marcoD': '/ivi/ilps/personal/akrasak/data/collections/cast21sep/msmarco-docs.tsv.int',
                        'KILT': '/ivi/ilps/personal/akrasak/data/collections/cast21sep/kilt_knowledgesource.tsv.int',
                        'WAPO': '/ivi/ilps/personal/akrasak/data/collections/cast21sep/wapo.tsv_incomplete.int',
                        }
 
-path_collection_mappings = {'CAR':'/ivi/ilps/personal/akrasak/data/collections/car-wiki2020-01-01/Car_collection.tsv.intmapping',
+path_collection_mappings = {
+                            # check examples in `data/collection_samples/msmarco-docs.tsv.sample.intmapping`
+                            'CAR':'/ivi/ilps/personal/akrasak/data/collections/car-wiki2020-01-01/Car_collection.tsv.intmapping',
                             'marcoP': '/ivi/ilps/personal/akrasak/data/collections/msmarco-passage/collection.tsv.intmapping',
                             'marcoD': '/ivi/ilps/personal/akrasak/data/collections/cast21sep/msmarco-docs.tsv.intmapping',
                             'KILT': '/ivi/ilps/personal/akrasak/data/collections/cast21sep/kilt_knowledgesource.tsv.intmapping',
@@ -109,31 +94,32 @@ path_collection_mappings = {'CAR':'/ivi/ilps/personal/akrasak/data/collections/c
                            }
 
 # Colbert paths
-path_colbert_checkpoint = '/ivi/ilps/personal/akrasak/data/models/colbert-400000.dnn'
-path_index_root = '/ivi/ilps/personal/akrasak/data/faiss_indexes/'
+path_colbert_checkpoint = '/ivi/ilps/personal/akrasak/data/models/colbert-400000.dnn' #TODO: download colbert checkpoint and point to this path `colbert-400000.dnn`
 
-index_names = {'marcoP': 'MSMARCO.L2.32x200k.180len',
+# FAISS index paths (generated after ColBERT indexing)
+path_index_root = '/ivi/ilps/personal/akrasak/data/faiss_indexes/' # TODO: replace with parent FAISS index folder
+index_names = {'marcoP': 'MSMARCO.L2.32x200k.180len', # the FAISS index of MSMarco should be @ `faiss_indexes/MSMARCO.L2.32x200k.180len/`
                'CAR':'CAR.FirstP.L2.32x200k.180len',
                'marcoD': 'marco.FirstP.L2.32x200k.180len',
                'KILT': 'kilt.FirstP.L2.32x200k.180len',
                'WAPO': 'wapo.FirstP.L2.32x200k.180len',
                }
 
-# path rankings
+# path rankings: those are the output ranking files (needed for the analysis section)
 path_rankings_noexp = {
-                'cast19': {'raw': '/home/akrasak/cqa-rewrite/ColBERT/experiments/cast19-raw-noexp/python_pipeline.py/2021-11-26_17.28.54/postprocessed_run.trecrun',
-                           'ctx': '/home/akrasak/cqa-rewrite/ColBERT/experiments/cast19-lastturn-noexp/python_pipeline.py/2021-11-26_19.38.37/postprocessed_run.trecrun',
-                           'allHist': '/home/akrasak/cqa-rewrite/ColBERT/experiments/cast19-allhist-noexp/python_pipeline.py/2021-11-29_11.18.49/postprocessed_run.trecrun',
-                           'human': '/home/akrasak/cqa-rewrite/ColBERT/experiments/cast19-human-noexp/python_pipeline.py/2021-12-06_17.32.17/postprocessed_run.trecrun'
+                'cast19': {'raw': 'data/rankings/cast19-raw-noexp.trecrun',
+                           'ctx': 'data/rankings/cast19-lastturn-noexp.trecrun',
+                           'allHist': 'data/rankings/cast19-allhist-noexp.trecrun',
+                           'human': 'data/rankings/postprocessed_run.trecrun'
                            },
-                'cast20': {'raw': '/home/akrasak/cqa-rewrite/ColBERT/experiments/cast20-raw-noexp/python_pipeline.py/2021-12-06_10.00.38/postprocessed_run.trecrun',
-                           'ctx': '/home/akrasak/cqa-rewrite/ColBERT/experiments/cast20-last_turn-noexp/python_pipeline.py/2021-12-06_13.44.50/postprocessed_run.trecrun',
-                           'allHist': '/home/akrasak/cqa-rewrite/ColBERT/experiments/cast20-allhist-noexp/python_pipeline.py/2021-12-07_12.04.15/postprocessed_run.trecrun',
-                           'human': '/home/akrasak/cqa-rewrite/ColBERT/experiments/cast20-human-noexp/python_pipeline.py/2021-12-06_18.38.18/postprocessed_run.trecrun',
+                'cast20': {'raw': 'data/rankings/cast20-raw-noexp.trecrun',
+                           'ctx': 'data/rankings/cast20-last_turn-noexp.trecrun',
+                           'allHist': 'data/rankings/cast20-allhist-noexp.trecrun',
+                           'human': 'data/rankings/cast20-human-noexp.trecrun',
                            },
-                'cast21': {'raw': '/home/akrasak/cqa-rewrite/ColBERT/experiments/cast21-raw-noexp/python_pipeline.py/2021-11-30_13.39.36/postprocessed_run.trecrun',
-                           'ctx': '/home/akrasak/cqa-rewrite/ColBERT/experiments/cast21-lastturn-noexp/python_pipeline.py/2021-11-29_13.15.12/postprocessed_run.trecrun',
-                           'allHist': '/home/akrasak/cqa-rewrite/ColBERT/experiments/cast21-allhist-noexp/python_pipeline.py/2021-11-30_23.34.34/postprocessed_run.trecrun',
-                           'human': '/home/akrasak/cqa-rewrite/ColBERT/experiments/cast21-human-noexp/python_pipeline.py/2021-12-06_19.44.48/postprocessed_run.trecrun',
+                'cast21': {'raw': 'data/rankings/cast21-raw-noexp.trecrun',
+                           'ctx': 'data/rankings/cast21-lastturn-noexp.trecrun',
+                           'allHist': 'data/rankings/cast21-allhist-noexp.trecrun',
+                           'human': 'data/rankings/cast21-human-noexp.trecrun',
                            }
 }
